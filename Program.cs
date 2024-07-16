f08266a1f8420eb13b1095685b4fae24d5b88f38
Mariojunioor <marioluciosantosjunior@outlook.com>
2024-07-16 11:17:53 -0300

    <CHORE> Melhora de Layout

--- Program.cs
+++ Program.cs
@@ -4,6 +4,16 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddSignalR();
 
+// Configurar Kestrel para escutar em portas específicas
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.ListenLocalhost(5000); // Porta HTTP
+    options.ListenLocalhost(5001, listenOptions =>
+    {
+        listenOptions.UseHttps(); // Porta HTTPS
+    });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
