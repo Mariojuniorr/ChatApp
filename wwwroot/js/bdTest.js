f5b1339da203b49f08e0b3f678c71f11a6176cdf
Mariojunioor <marioluciosantosjunior@outlook.com>
2024-07-24 18:17:25 -0300

    Testing JSON/DataBase

---
+++ wwwroot/js/bdTest.js
@@ -0,0 +1,22 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+let product_name = document.querySelector("#product_name");
+
+function fetchJSONData() {
+    fetch('./json/testProducts.json')
+        .then((res) => {
+            if (!res.ok) {
+                throw new Error(`Entre em contato com o administrador! Error Status: ${res.status}`);
+            }
+            return res.json();
+        })
+        .then((data) => data.Products.map((product) => {
+            console.log(product);
+        })
+        .catch((error) => console.error("Não foi possível carregar JSON", error)));
+}
+
+fetchJSONData();
